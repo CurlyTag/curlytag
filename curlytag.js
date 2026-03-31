40698a1c5a571d03da416bd60d9b10df3018deb5
Anton Semenov <anton.a.semenov@proton.me>
2026-03-31 23:47:23 +0300

    fix: add unless to openclose map

--- curlytag.js
+++ curlytag.js
@@ -69,6 +69,9 @@ class CurlyTag {
             ],
             comment: [
                 'endcomment'
+            ],
+            unless: [
+                'endunless'
             ]
         };
 

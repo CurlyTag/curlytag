d0afead2071b73a33af4c80b364b512d6dc7e37f
Anton Semenov <anton.a.semenov@proton.me>
2026-04-16 18:44:41 +0000

    fix: correct fetch() path construction for namespaces

--- curlytag.js
+++ curlytag.js
@@ -322,7 +322,7 @@ class CurlyTag {
             }
 
             if (this.path.has(namespace)) {
-                file = this.path.get(namespace) + path.substr(path, namespace.length) + '.html';
+                file = this.path.get(namespace) + path.substr(namespace.length) + '.html';
             }
         }
 
